7a0303dbe1d4122bba4fab58f49933209c6297b1
Anakin Sun <anakin.sun@chope.co>
2019-07-26 13:51:03 +0800

    add new files

---
+++ demo4/user-api/handler/user.go
@@ -0,0 +1,73 @@
+package handler
+
+import (
+	"demo4/lib/tracer"
+	"demo4/lib/wrapper/tracer/opentracing/gin2micro"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+
+	usersrv "demo4/user-service/proto/user"
+
+	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/client"
+)
+
+const UserServiceName = "chope.co.srv.user"
+
+type UserServiceHandler struct {
+	userS usersrv.UserService
+}
+
+func New(client client.Client) *UserServiceHandler {
+	return &UserServiceHandler{
+		userS: usersrv.NewUserService(UserServiceName, client),
+	}
+}
+
+func (u *UserServiceHandler) Info(c *gin.Context) {
+
+	req := &usersrv.User{Id: 1}
+	ctx, ok := gin2micro.ContextWithSpan(c)
+	if !ok {
+		logrus.Error("context error")
+	}
+	res, err := u.userS.Get(ctx, req)
+	tracer.Trace(ctx, "Info", req, res, err)
+	if err != nil {
+		logrus.Error(err)
+	}
+	c.JSON(http.StatusOK, res)
+}
+
+func (u *UserServiceHandler) Create(c *gin.Context) {
+
+	req := &usersrv.User{Name: "anakin", Password: "123456", Company: "chope", Email: "anakin.sun@chope.co"}
+	ctx, ok := gin2micro.ContextWithSpan(c)
+	if !ok {
+		logrus.Error("context error")
+	}
+	res, err := u.userS.Create(ctx, req)
+	if err != nil {
+		logrus.Error(err)
+	}
+	tracer.Trace(ctx, "Create", req, res, err)
+	c.JSON(http.StatusOK, res)
+}
+
+func (u *UserServiceHandler) Auth(c *gin.Context) {
+	req := &usersrv.User{
+		Email:    "anakin.sun@chope.co",
+		Password: "123456",
+	}
+	ctx, ok := gin2micro.ContextWithSpan(c)
+	if !ok {
+		logrus.Error("context error")
+	}
+	res, err := u.userS.Auth(ctx, req)
+	tracer.Trace(ctx, "Auth", req, res, err)
+	if err != nil {
+		logrus.Error(err)
+	}
+	c.JSON(http.StatusOK, res)
+}
---
+++ demo4/user-api/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"demo4/lib/tracer"
+	"demo4/lib/wrapper/tracer/opentracing/gin2micro"
+	"demo4/user-api/handler"
+	"net/http"
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+
+	"github.com/afex/hystrix-go/hystrix"
+	"github.com/sirupsen/logrus"
+
+	hystrixplugin "github.com/micro/go-plugins/wrapper/breaker/hystrix"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
+	"demo4/middleware"
+
+	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/registry/consul"
+	"github.com/micro/go-micro/service/grpc"
+	"github.com/micro/go-micro/web"
+)
+
+const ServiceName = "chope.co.api.user"
+
+func init() {
+	middleware.InitWithFile(".env.json")
+	logrus.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat: "2006-01-02T15:04:05.000",
+		FullTimestamp:   true,
+	})
+}
+
+func main() {
+
+	gin2micro.SetSamplingFrequency(50)
+	t, io, err := tracer.NewTracer(ServiceName)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer io.Close()
+
+	opentracing.SetGlobalTracer(t)
+
+	//registry
+	reg := consul.NewRegistry()
+	srv := web.NewService(
+		web.Name(ServiceName),
+		web.MicroService(grpc.NewService()),
+		web.Registry(reg),
+		web.RegisterTTL(time.Second*15),
+		web.RegisterInterval(time.Second*10),
+	)
+
+	_ = srv.Init()
+
+	hystrix.DefaultTimeout = 5000
+
+	sClient := hystrixplugin.NewClientWrapper()(srv.Options().Service.Client())
+	_ = sClient.Init(
+		client.Retries(3),
+		client.Retry(func(ctx context.Context, req client.Request, retryCount int, err error) (bool, error) {
+			logrus.Info(req.Method(), retryCount, " client retry")
+			return true, nil
+		}),
+	)
+
+	h := handler.New(sClient)
+	router := gin.Default()
+	r := router.Group("/user")
+	r.Use(gin2micro.TracerWrapper)
+	r.GET("/info", h.Info)
+	r.POST("/create", h.Create)
+	r.POST("/auth", h.Auth)
+	srv.Handle("/", router)
+	err = srv.Run()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	//PrometheusBoot()
+}
+func PrometheusBoot() {
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		err := http.ListenAndServe(":8085", nil)
+		if err != nil {
+			logrus.Println(err)
+		}
+	}()
+}
